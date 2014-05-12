79878541bef8676b8a8d481dad67695a68802250
Ian Daniher <it.daniher@gmail.com>
2014-05-12 19:52:11 -0400

    flesh out subgrpah definition creation

--- stage2.rs
+++ stage2.rs
@@ -46,8 +46,8 @@ fn expandPrim(nodepname: ~str) -> ~str {
 fn getDefaultArgs(nodepname: ~str) -> ~str {
 	match nodepname.len() {
 		1 => match nodepname.char_at(0) {
-			'*' => ~"1.0",
-			'+' | 'Z' => ~"0.0",
+			'*' => ~"1.0f32",
+			'+' | 'Z' => ~"0.0f32",
 			'?' => ~"|a, b| {a.map(|x| {match x {Some(y) => b.send(y), None => ()}}).last();()}",
 			_ => ~"",
 		},
@@ -80,13 +80,41 @@ fn genFunction(g: Graph, args: Vec<json::Json>) -> ast::Item {
 	let mut channelStmts: Vec<ast::P<ast::Stmt>> = vec!();
 	let mut spawnExprs: Vec<ast::P<ast::Expr>> = vec!();
 	let mut fnargv: Vec<ast::Arg> = vec!();
+	let mut io: Vec<~str> = vec!();
 	// this does the work - iterate over nodes and arguments
+	for (uid, node) in g.nodes.clone().move_iter() {
+		let mut rxers: Vec<~str> = vec!();
+		let mut txers: Vec<~str> = vec!();
+		let n = expandPrim(node.pname.clone());
+		for edge in g.edges.iter() {
+			if &uid == edge.get(0) {
+				txers.push("tx".to_owned().append(edge.get(0).slice_from(0)).append(edge.get(1).slice_from(0)));
+			}
+			else if &uid == edge.get(1) {
+				rxers.push("rx".to_owned().append(edge.get(0).slice_from(0)).append(edge.get(1).slice_from(0)));
+			}
+		}
+		match n.slice_from(0) {
+			"in" => {
+				let x = (~"r").append(txers.get(0).slice_from(1));
+				io.push(x.clone());
+				fnargv.push(abstrast::arg(x, "Receiver<f32>"))
+				}
+			"out" => {
+				let x = (~"t").append(rxers.get(0).slice_from(1));
+				io.push(x.clone());
+				fnargv.push(abstrast::arg(x, "Sender<f32>"))
+				}
+			_ => {}
+		}
+	}
 	for ((uid, node), arg) in g.nodes.clone().move_iter().zip(args.move_iter()) {
 		let mut rxers: Vec<~str> = vec!();
 		let mut txers: Vec<~str> = vec!();
 		for edge in g.edges.iter() {
 			if &uid == edge.get(0) {
-				txers.push("tx".to_owned().append(edge.get(0).slice_from(0)).append(edge.get(1).slice_from(0)));
+				let ename = "tx".to_owned().append(edge.get(0).slice_from(0)).append(edge.get(1).slice_from(0));
+				txers.push(ename);
 			}
 			else if &uid == edge.get(1) {
 				rxers.push("rx".to_owned().append(edge.get(0).slice_from(0)).append(edge.get(1).slice_from(0)));
@@ -108,8 +136,9 @@ fn genFunction(g: Graph, args: Vec<json::Json>) -> ast::Item {
 					spawnExprs.push(spawn(~"fork",
 						vec!(expr_path(frx), expr_owned_vec(txers.iter().map(
 							|x| expr_path(x.slice_from(0))).collect()))));
-					channelStmts.push(stmt_let(pat_tuple(vec!(pat_name(ftx.clone()),
-						pat_name(frx.clone()))), expr_call(expr_path("channel"), vec!())));
+					println!("{}", (&io, &ftx, &txers));
+						channelStmts.push(stmt_let(pat_tuple(vec!(pat_name(ftx.clone()),
+							pat_name(frx.clone()))), expr_call(expr_path("channel"), vec!())));
 					argv.push(expr_path(ftx))
 				}
 			};
@@ -119,31 +148,31 @@ fn genFunction(g: Graph, args: Vec<json::Json>) -> ast::Item {
 				Some(lits) => {
 					match lits {
 						ast::ExprVec(v) => if v.len() > 0 {v} else {
-							match getDefaultArgs(node.pname.clone()).slice_from(0) {
-								"" => vec!(),
-								x => vec!(parse_expr(x))
-							}
-						},
+						match getDefaultArgs(node.pname.clone()).slice_from(0) {
+							"" => vec!(),
+							x => vec!(parse_expr(x))
+						}},
 						ast::ExprPath(_) | ast::ExprVstore(_, _) => vec!(expr(lits)),
 						_ => fail!("{:?}", lits)
-				}}
+					}
+				}
 				None => vec!()
-			}
-		);
+			});
 		match n.slice_from(0) {
-			"in" => {fnargv.push(abstrast::arg((~"r").append(txers.get(0).slice_from(1)), "Receiver<f32>"))}
-			"out" => {fnargv.push(abstrast::arg((~"t").append(rxers.get(0).slice_from(1)), "Sender<f32>"))}
+			"in" => {},
+			"out" => {},
 			_ => {
-			spawnExprs.push(spawn(n, argv));
-			txers.iter().map(|txer| {
-				let dstrm = (~"r").append((*txer).slice_from(1));
-				channelStmts.push(stmt_let(pat_tuple(vec!(pat_name(*txer),
-					pat_name(dstrm))), expr_call(expr_path("channel"), vec!())))
-			}).last();
+					spawnExprs.push(spawn(n, argv));
+					txers.iter().map(|txer| {
+						let dstrm = (~"r").append((*txer).slice_from(1));
+						if io.clone().iter().filter(|x| x.slice_from(0) == txer.slice_from(0)).len() < 1 {
+							channelStmts.push(stmt_let(pat_tuple(vec!(pat_name(*txer),
+								pat_name(dstrm))), expr_call(expr_path("channel"), vec!())));
+							}
+						}).last();
+			}
 		}
-		};
-	}
-
+	};
 	channelStmts.push_all_move(spawnExprs.move_iter().map(|x| stmt_semi(x)).collect());
 	match g.args {
 		Some(ref aargs) => {aargs.iter().map(|x| fnargv.push(abstrast::arg(x.slice_from(0), "f32"))).last();},
