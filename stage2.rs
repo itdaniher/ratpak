8219247250ecab5555421ddd6ff0a1b3fd6e2b94
Ian Daniher <it.daniher@gmail.com>
2014-10-26 23:06:30 -0400

    fix all but three warnings

--- stage2.rs
+++ stage2.rs
@@ -13,7 +13,6 @@ use abstrast::*;
 use syntax::ast;
 use syntax::ptr;
 use serialize::{json, Encodable, Decodable};
-use std::str;
 
 #[deriving(Decodable,Encodable,Clone)]
 struct Node {
@@ -59,9 +58,9 @@ impl<'a> graphviz::GraphWalk<'a, Node, Edge> for Graph {
     fn target(&self, e: &Edge) -> Node { let &(_,ref t) = e; self.nodes.iter().filter_map(|n| {if &n.uid == t { Some(n.clone()) } else {None}}).next().unwrap() }
 }
 
-fn expandPrim(nodepname: String) -> String {
+fn expand_prim(nodepname: String) -> String {
 	let mut snp = nodepname.clone();
-	let c0 = snp.shift_char().unwrap();
+	let c0 = snp.remove(0).unwrap();
 	let mut out: String= "".to_string();
 	match c0 {
 		'*' => "mul",
@@ -75,25 +74,25 @@ fn expandPrim(nodepname: String) -> String {
 		'{' => "looper",
 		'&' => "crossApplicator",
 		'!' => "applicator",
-		'?' => {out = expandPrim(snp).append("Optional"); out.as_slice()},
-		'/' => {out = expandPrim(snp).append("Across"); out.as_slice()},
-		',' => {out = expandPrim(snp).append("Vecs"); out.as_slice()},
+		'?' => {out = expand_prim(snp); out.push_str("Optional"); out.as_slice()},
+		'/' => {out = expand_prim(snp); out.push_str("Across"); out.as_slice()},
+		',' => {out = expand_prim(snp); out.push_str("Vecs"); out.as_slice()},
 		 _  => nodepname.as_slice()
 	}.to_string()
 }
 
-fn getDefaultArgs(nodepname: String) -> String{
+fn get_default_args(nodepname: String) -> String{
 	let mut snp = nodepname.clone();
-	let c0 = snp.shift_char().unwrap();
+	let c0 = snp.remove(0).unwrap();
 	let mut out: String= "".to_string();
 	match nodepname.len() {
 		1...3 => match c0 {
 			'*' => "num::one()",
 			'+' | 'Z' => "num::zero()",
-			'/' => { out = getDefaultArgs(snp); out.as_slice()}
-			',' => { match getDefaultArgs(snp).as_slice() {
+			'/' => { out = get_default_args(snp); out.as_slice()}
+			',' => { match get_default_args(snp).as_slice() {
 					"" => "",
-					x => {out = "range(0,512).map(|_| ".to_string().append(x).append(").collect()"); out.as_slice()}
+					x => {out = "range(0,512).map(|_| ".to_string(); out.push_str(x); out.push_str(").collect()"); out.as_slice()}
 				}
 			}
 			_ => "",
@@ -102,7 +101,7 @@ fn getDefaultArgs(nodepname: String) -> String{
 	}.to_string()
 }
 
-fn getGraph() -> Vec<(Graph, Vec<json::Json>)> {
+fn get_graph() -> Vec<(Graph, Vec<json::Json>)> {
 	// get json graph and node arguments from stage2.json
 	let json_str_to_decode = File::open(&Path::new("./stage2.json")).read_to_string().unwrap();
 	let json_object = json::from_str(json_str_to_decode.as_slice()).unwrap();
@@ -114,82 +113,96 @@ fn getGraph() -> Vec<(Graph, Vec<json::Json>)> {
         Ok(v) => v,
 		Err(e) => fail!("json decoding error")
 	};
-	y.move_iter().zip(args.move_iter()).collect()
+	y.into_iter().zip(args.into_iter()).collect()
 }
 
 
-fn genFunction(g: Graph, args: Vec<json::Json>) -> ast::Item {
-	let mut channelStmts: Vec<ptr::P<ast::Stmt>> = vec!();
-	let mut spawnExprs: Vec<ptr::P<ast::Expr>> = vec!();
+fn gen_function(g: Graph, args: Vec<json::Json>) -> ast::Item {
+	let mut channel_stmts: Vec<ptr::P<ast::Stmt>> = vec!();
+	let mut spawn_exprs: Vec<ptr::P<ast::Expr>> = vec!();
 	let mut fnargv: Vec<ast::Arg> = vec!();
 	let mut io: Vec<String> = vec!();
 	// this does the work - iterate over nodes and arguments
-	for node in g.nodes.clone().move_iter() {
+	for node in g.nodes.clone().into_iter() {
 		let mut rxers: Vec<String> = vec!();
 		let mut txers: Vec<String> = vec!();
-		let n = expandPrim(node.pname.clone());
+		let n = expand_prim(node.pname.clone());
 		for &(ref e0, ref e1) in g.edges.iter() {
 			if &node.uid == e0 {
-				txers.push("tx".to_string().append(e0.as_slice()).append(e1.as_slice()));
+				let mut ename = "tx".to_string();
+				ename.push_str(e0.as_slice());
+				ename.push_str(e1.as_slice());
+				txers.push(ename);
 			}
 			else if &node.uid == e1 {
-				rxers.push("rx".to_string().append(e0.as_slice()).append(e1.as_slice()));
+				let mut ename = "rx".to_string();
+				ename.push_str(e0.as_slice());
+				ename.push_str(e1.as_slice());
+				rxers.push(ename);
 			}
 		}
 		match n.as_slice() {
 			"in" => {
-				let x = ("r").to_string().append(txers.get(0).as_slice().slice_from(1));
+				let mut x = "r".to_string();
+				x.push_str(txers[0].as_slice().slice_from(1));
 				io.push(x.clone());
 				fnargv.push(abstrast::arg(x.as_slice(), "Receiver<T>"))
 				}
 			"out" => {
-				let x = ("t").to_string().append(rxers.get(0).as_slice().slice_from(1));
-				io.push(x.clone());
+				let mut x = "t".to_string();
+				x.push_str(rxers[0].as_slice().slice_from(1));
 				fnargv.push(abstrast::arg(x.as_slice(), "Sender<T>"))
 				}
 			_ => {}
 		}
 	}
-	for (node, arg) in g.nodes.clone().move_iter().zip(args.move_iter()) {
-		let n = expandPrim(node.pname.clone());
+	for (node, arg) in g.nodes.clone().into_iter().zip(args.into_iter()) {
+		let n = expand_prim(node.pname.clone());
 		let mut rxers: Vec<String> = vec!();
 		let mut txers: Vec<String> = vec!();
 		let mut argv = vec!();
 		for &(ref e0, ref e1) in g.edges.iter() {
 			if &node.uid == e0 {
-				let ename = "tx".to_string().append(e0.as_slice()).append(e1.as_slice());
+				let mut ename = "tx".to_string();
+				ename.push_str(e0.as_slice());
+				ename.push_str(e1.as_slice());
 				txers.push(ename);
 			}
 			else if &node.uid == e1 {
-				rxers.push("rx".to_string().append(e0.as_slice()).append(e1.as_slice()));
+				let mut ename = "rx".to_string();
+				ename.push_str(e0.as_slice());
+				ename.push_str(e1.as_slice());
+				rxers.push(ename);
 			}
 		}
 		match rxers.len() {
 			0 => (),
-			1 => argv.push(expr_path(rxers.get(0).as_slice())),
+			1 => argv.push(expr_path(rxers[0].as_slice())),
 			_ => argv.push(expr_vec(rxers.iter().map(|x| expr_path(x.as_slice())).collect()))
 		}
 		match txers.len() {
 			0 => (),
-			1 => argv.push(expr_path(txers.get(0).as_slice())),
+			1 => argv.push(expr_path(txers[0].as_slice())),
 			_ => {
-					let ftx = txers.get(0).as_slice().slice_to(13).to_string().append("0");
-					let frx = ("r").to_string().append(ftx.as_slice().slice_from(1));
-					spawnExprs.push(spawn("fork",
+					let mut ftx = txers[0].as_slice().slice_to(13).to_string();
+					ftx.push_str("0");
+					let mut frx = ("r").to_string();
+					frx.push_str(ftx.as_slice().slice_from(1));
+					spawn_exprs.push(spawn("fork",
 						vec!(expr_path(frx.as_slice()), expr_vec(txers.iter().map(
 							|x| expr_path(x.as_slice())).collect()))));
-						channelStmts.push(stmt_let(pat_tuple(vec!(pat_name(ftx.as_slice()),
+						channel_stmts.push(stmt_let(pat_tuple(vec!(pat_name(ftx.as_slice()),
 							pat_name(frx.as_slice()))), expr_call(expr_path("channel"), vec!())));
 					argv.push(expr_path(ftx.as_slice()))
 				}
 			};
 
-		argv.push_all_move(
+		argv.extend(
 			match JSONtoAST(arg.clone()) {
 				Some(lits) => {
 					match lits {
 						ast::ExprVec(v) => if v.len() > 0 {v} else {
-						match getDefaultArgs(node.pname.clone()).as_slice() {
+						match get_default_args(node.pname.clone()).as_slice() {
 							"" => vec!(),
 							x => vec!(parse_expr(x.to_string()))
 						}},
@@ -198,40 +211,43 @@ fn genFunction(g: Graph, args: Vec<json::Json>) -> ast::Item {
 					}
 				}
 				None => vec!()
-			});
+			}.into_iter());
 		match n.as_slice() {
 			"in" => {},
 			"out" => {},
 			_ => {
-					spawnExprs.push(spawn(n.as_slice(), argv));
+					spawn_exprs.push(spawn(n.as_slice(), argv));
 					txers.iter().map(|txer| {
-						let dstrm = ("r").to_string().append(txer.as_slice().slice_from(1));
+						let mut dstrm = ("r").to_string();
+						dstrm.push_str(txer.as_slice().slice_from(1));
 						if io.clone().iter().filter(|x| x.as_slice() == txer.as_slice()).count() < 1 {
-							channelStmts.push(stmt_let(pat_tuple(vec!(pat_name(txer.as_slice()),
+							channel_stmts.push(stmt_let(pat_tuple(vec!(pat_name(txer.as_slice()),
 								pat_name(dstrm.as_slice()))), expr_call(expr_path("channel"), vec!())));
 							}
 						}).last();
 			}
 		}
 	};
-	channelStmts.push_all_move(spawnExprs.move_iter().map(|x| stmt_semi(x)).collect());
+	channel_stmts.extend(spawn_exprs.into_iter().map(|x| stmt_semi(x)));
 	match g.args {
 		Some(ref aargs) => {aargs.iter().map(|x| fnargv.push(abstrast::arg(x.as_slice(), "T"))).last();},
 		None => ()
 	};
-	fn_item(g.name.as_slice(), fnargv, ty_nil(), block(channelStmts, None))
+	fn_item(g.name.as_slice(), fnargv, ty_nil(), block(channel_stmts, None))
 }
 
 fn main () {
-	let forest: Vec<(Graph, Vec<json::Json>)> = getGraph();
+	let forest: Vec<(Graph, Vec<json::Json>)> = get_graph();
 	for &(ref g, _) in forest.iter() {
-		let mut out = File::create(&Path::new(g.name.clone().append(".dot"))).unwrap();
-		graphviz::render(g, &mut out);
+		let mut fname = g.name.clone();
+		fname.push_str(".dot");
+		let mut out = File::create(&Path::new(fname)).unwrap();
+		graphviz::render(g, &mut out).unwrap();
 	}
 	let mut stage3 = File::create(&Path::new("stage3.rs")).unwrap();
 	let boilerplate = File::open(&Path::new("./boilerplate.rs")).read_to_end().unwrap();
-	stage3.write(boilerplate.as_slice());
-	for z in forest.move_iter().map(|(x,y)| genFunction(x,y)) {
-		 stage3.write(syntax::print::pprust::item_to_string(&z).as_bytes());
+	stage3.write(boilerplate.as_slice()).unwrap();
+	for z in forest.into_iter().map(|(x,y)| gen_function(x,y)) {
+		 stage3.write(syntax::print::pprust::item_to_string(&z).as_bytes()).unwrap();
 	}
 }
